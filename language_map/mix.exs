cc97c161d74b9ccdb6eff5bcfc4ecbb147818a40
Peter Caisse <pcaisse@gmail.com>
2018-03-11 14:13:15 -0400

    Add Dialyxer

--- language_map/mix.exs
+++ language_map/mix.exs
@@ -40,7 +40,8 @@ defmodule LanguageMap.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 
