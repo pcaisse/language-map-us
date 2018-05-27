45c11cd37eeea0eff53b31ce1eaba775b9200e06
Peter Caisse <pcaisse@gmail.com>
2018-05-27 17:31:38 -0400

    Simlify frontend logic and clean up styles

--- language_map/priv/static/js/main.js
+++ language_map/priv/static/js/main.js
@@ -1,13 +1,6 @@
 const mapCenter = [37.69164172115731, -94.65820312500001];
 const map = L.map('map').setView(mapCenter, 5);
 
-// NOTE: Response to get GeoJSON for all states is saved as a static file since
-// it takes ~10s to calculate (too long to wait) though the payload is only
-// 500KB. This is because the query involves the simplification of the geometry
-// which brings down the file size but takes more time. This simplification
-// isn't noticable until you zoom in more, at which point we've already
-// switched to showing PUMA geometries.
-
 let layers;
 
 function drawTiles() {
@@ -33,23 +26,22 @@ function fetchJSON(url) {
 function createLayers(results) {
   return L.layerGroup(
     results.map(result => {
-      return L.geoJSON(result.geom).on("click", e => {
+      return L.geoJSON(result.geom, {
+        color: 'purple',
+        fillOpacity: 0
+      }).on("click", e => {
         return map.fitBounds(e.layer.getBounds());
       });
     })
   );
 }
 
-function drawMap(isStateLevel) {
+function drawMap() {
   // NOTE: Query params should be the same for both async requests
   const search = window.location.search;
   fetchJSON('/api/speakers/' + search).then(speakerResults => {
     // We have speaker data. Now we need layers that reflect that data.
-    if (isStateLevel) {
-      return fetchJSON('static/json/states.json');
-    } else {
-      return fetchJSON('/api/geojson/' + search);
-    }
+    return fetchJSON('/api/geojson/' + search);
   }).then(geojsonResults => {
     // TODO: Use speakerResults
     if (layers) {
