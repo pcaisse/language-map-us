5b69cd17811dc53fe47fe71981308546a83d14bb
Peter Caisse <pcaisse@gmail.com>
2018-07-01 17:49:11 -0400

    Fix env vars for db container

--- language_map/config/test.exs
+++ language_map/config/test.exs
@@ -6,7 +6,7 @@ config :language_map, LanguageMap.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("POSTGRES_USER"),
   password: System.get_env("POSTGRES_PASSWORD"),
-  database: "language_map_test",
+  database: System.get_env("POSTGRES_DB") <> "_test",
   hostname: System.get_env("POSTGRES_HOST"),
   pool: Ecto.Adapters.SQL.Sandbox,
   types: LanguageMap.PostgresTypes
