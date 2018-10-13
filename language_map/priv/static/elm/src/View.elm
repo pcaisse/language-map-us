a986f4ab33decc24ce77bb64a43a3ba8db1ee2dd
Peter Caisse <pcaisse@gmail.com>
2018-10-13 14:06:11 -0400

    Elm view just renders map div

--- language_map/priv/static/elm/src/View.elm
+++ language_map/priv/static/elm/src/View.elm
@@ -1,23 +1,11 @@
 module View exposing (view)
 
 import Html exposing (Html, div, text)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (id)
 import Model exposing (Model, Msg(..), BoundingBox)
-
-
-showBoundingBox : BoundingBox -> String
-showBoundingBox boundingBox =
-    let
-        { southwestLat, southwestLng, northeastLat, northeastLng } =
-            boundingBox
-    in
-        String.join ", "
-            ([ southwestLat, southwestLng, northeastLng, northeastLng ]
-                |> List.map toString
-            )
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "main" ]
-        [ text (showBoundingBox model.filters.boundingBox) ]
+    div [ id "map" ]
+        []
