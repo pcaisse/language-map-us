b22418553683b895a17e6d34b9e7ab615a1e55e0
Peter Caisse <pcaisse@gmail.com>
2018-11-25 12:09:43 -0500

    Add prod config file

---
+++ language_map/config/prod.exs
@@ -0,0 +1,11 @@
+use Mix.Config
+
+config :language_map, ecto_repos: [LanguageMap.Repo]
+
+config :language_map, LanguageMap.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  database: System.get_env("POSTGRES_DB"),
+  hostname: System.get_env("POSTGRES_HOST"),
+  types: LanguageMap.PostgresTypes
