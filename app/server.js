e461fa0120bda8f13c1d6c0e8f909f139975b3f8
Peter Caisse <pcaisse@gmail.com>
2023-02-25 16:42:28 -0500

    Add Cypress tests

--- app/server.js
+++ app/server.js
@@ -3,17 +3,21 @@
  */
 const express = require("express");
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 const path = require("path");
 
+const tileHeaders = {
+  setHeaders: function (res, path) {
+    if (path.endsWith(".pbf")) {
+      res.setHeader("Content-Encoding", "gzip");
+    }
+  },
+};
+
+app.use(express.static(path.join(__dirname, "static"), tileHeaders));
 app.use(
-  express.static(path.join(__dirname, "static"), {
-    setHeaders: function (res, path) {
-      if (path.endsWith(".pbf")) {
-        res.setHeader("Content-Encoding", "gzip");
-      }
-    },
-  })
+  "/test-tiles",
+  express.static(path.join(__dirname, "test-tiles"), tileHeaders)
 );
 
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
